81d285f7b2f2cd3f6d9a20e432e0442238decb98
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-08-19 15:09:36 +0530

    09: auth states

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,21 +1,27 @@
 "use client";
 
 import { useState } from "react";
-import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 import { CategoriesSidebar } from "./categories-sidebar";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 interface Props {
     disabled?: boolean;
 };
 
-export const SearchInput = ({ 
+export const SearchInput = ({
     disabled,
 }: Props) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const trpc = useTRPC();
+    const session = useQuery(trpc.auth.session.queryOptions());
 
     return (
         <div className="flex items-center gap-2 w-full">
@@ -24,7 +30,6 @@ export const SearchInput = ({
                 <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
                 <Input className="pl-8" placeholder="Search products" disabled={disabled} />
             </div>
-            {/* TODO: Add categories view all button */}
             <Button
                 variant="elevated"
                 className="size-12 shrink-0 flex lg:hidden"
@@ -32,6 +37,17 @@ export const SearchInput = ({
             >
                 <ListFilterIcon />
             </Button>
+            {session.data?.user && (
+                <Button
+                    asChild
+                    variant="elevated"
+                >
+                    <Link href="/library">
+                        <BookmarkCheckIcon />
+                        Library
+                    </Link>
+                </Button>
+            )}
             {/* TODO: Add library button */}
         </div>
     );
