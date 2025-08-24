6823c5df807eec330375599a27b7dd6b203c2141
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-08-24 18:11:26 +0530

    13: api filters and sorting

--- src/modules/products/search-params.ts
+++ src/modules/products/search-params.ts
@@ -1,6 +1,6 @@
-import { useQueryStates, parseAsArrayOf, parseAsString, parseAsStringLiteral } from "nuqs";
+import { createLoader, parseAsArrayOf, parseAsString, parseAsStringLiteral } from "nuqs/server";
 
-const sortValues = ["curated", "trending", "hot_and_new"] as const;
+export const sortValues = ["curated", "trending", "hot_and_new"] as const;
 
 const params = {
     sort: parseAsStringLiteral(sortValues).withDefault("curated"),
@@ -21,6 +21,4 @@ const params = {
         .withDefault([]),
 };
 
-export const useProductFilters = () => {
-    return useQueryStates(params);
-};
+export const loadProductFilters = createLoader(params);
