8474b6bd059ef25e82adc8c8820a94a92da2bbeb
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-08-30 22:55:50 +0530

    19: checkout page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,12 +4,14 @@ import { tagsRouter } from '@/modules/tags/server/procedures';
 import { authRouter } from '@/modules/auth/server/procedures';
 import { tenantsRouter } from '@/modules/tenants/server/procedures';
 import { productsRouter } from '@/modules/products/server/procedures';
+import { checkoutsRouter } from '@/modules/checkout/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutsRouter,
   products: productsRouter,
   categories: categoriesRouter,
 });
