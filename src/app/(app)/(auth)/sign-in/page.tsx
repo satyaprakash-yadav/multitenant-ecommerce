ac1ef738c8da89b3e5ec88ce501a989e7cf9f2c6
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-09-12 17:19:54 +0530

    28: deployment

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 
+export const dynamic = "force-dynamic";
+
 const Page = async () => {
     const session = await caller.auth.session();
     
